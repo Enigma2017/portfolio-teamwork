8b08be228a597357630c22401590031c06a27f5f
vladabel12 <vladabel12@gmail.com>
2024-05-09 17:35:29 +0300

    added blocks

--- src/sections/recommendations.jsx
+++ src/sections/recommendations.jsx
@@ -4,9 +4,26 @@ function Recommendations() {
 
     return (
       <div className="recommendations">
-        <div className="recommendations__main-title">Recommendations</div>
-        <div className="recommendations__description">Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. lorem ipsum</div>
-        <div className="recommendations__container">
+        <div className="recommendations__main-container">
+          <div className="recommendations__main-title">Recommendations</div>
+          <div className="recommendations__main-desc">
+            <div className="recommendations__description">Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. lorem ipsum</div>
+          </div>
+          <div className="recommendations__container">
+            <div className="recommendations__block">
+              <div className="recommendations__stars">
+                <RecommendationsSrar />
+                <RecommendationsSrar />
+                <RecommendationsSrar />
+                <RecommendationsSrar />
+                <RecommendationsSrar />
+              </div>
+              <div className="recommendations__block-title">Great Quality!</div>
+              <div className="recommendations__block-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vitae nulla diam in ac dictum a urna viverra morbi. Morbi donec amet....</div>
+              <div className="recommendations__character">
+                <img src="/public/recommendationsImg/james.svg" alt="" />
+              </div>
+            </div>
           <div className="recommendations__block">
             <div className="recommendations__stars">
               <RecommendationsSrar />
@@ -16,7 +33,20 @@ function Recommendations() {
               <RecommendationsSrar />
             </div>
             <div className="recommendations__block-title">Great Quality!</div>
+            <div className="recommendations__block-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vitae nulla diam in ac dictum a urna viverra morbi. Morbi donec amet....</div>
           </div>
+          <div className="recommendations__block">
+            <div className="recommendations__stars">
+              <RecommendationsSrar />
+              <RecommendationsSrar />
+              <RecommendationsSrar />
+              <RecommendationsSrar />
+              <RecommendationsSrar />
+            </div>
+            <div className="recommendations__block-title">Great Quality!</div>
+            <div className="recommendations__block-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vitae nulla diam in ac dictum a urna viverra morbi. Morbi donec amet....</div>
+          </div>
+        </div>
         </div>
       </div>
     );
