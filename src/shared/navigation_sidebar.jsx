324a481a97d8f163d11ca47fa4769ad49b286500
Random4eg <panaram452@gmail.com>
2024-05-09 17:42:45 +0300

    added education and work element

--- src/shared/navigation_sidebar.jsx
+++ src/shared/navigation_sidebar.jsx
@@ -1,10 +1,12 @@
-function NavigationSidebar() {
+import DarkModeToggle from "./dark_mode"
 
+function NavigationSidebar() {
   return (
     <div className="navigation">
+      <DarkModeToggle />
       navigation!
     </div>
-  );
+  )
 }
 
-export default NavigationSidebar;
+export default NavigationSidebar
