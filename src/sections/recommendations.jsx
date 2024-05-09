843a3b81bcd78eeda2029a57deab287b63307264
vladabel12 <vladabel12@gmail.com>
2024-05-09 15:10:40 +0300

    added title

--- src/sections/recommendations.jsx
+++ src/sections/recommendations.jsx
@@ -1,8 +1,23 @@
+import { RecommendationsSrar } from "../../public/recommendationsImg/RecommendationsSrar";
+
 function Recommendations() {
 
     return (
       <div className="recommendations">
-        Working with Recommendations section
+        <div className="recommendations__main-title">Recommendations</div>
+        <div className="recommendations__description">Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. lorem ipsum</div>
+        <div className="recommendations__container">
+          <div className="recommendations__block">
+            <div className="recommendations__stars">
+              <RecommendationsSrar />
+              <RecommendationsSrar />
+              <RecommendationsSrar />
+              <RecommendationsSrar />
+              <RecommendationsSrar />
+            </div>
+            <div className="recommendations__block-title">Great Quality!</div>
+          </div>
+        </div>
       </div>
     );
   }
